4bec7a3cca80ac39a3eb4b001403c43b1ed6d6ea
numberTuu <65688015+numberTuu@users.noreply.github.com>
2020-06-11 11:59:27 -0700

    Modified with plot

--- src/w2v test/gensim-test.py
+++ src/w2v test/gensim-test.py
@@ -4,6 +4,8 @@ import pandas as pd
 import re 
 import string 
 import nltk
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
 
 #nltk.download('punkt') 
 
@@ -25,6 +27,7 @@ data.to_csv("newmeta.csv", index = False)
 data = pd.read_csv('newmeta.csv', low_memory= False)
 
 #Remove punctuation and lower case all characters
+#No removal of numbers
 def clean(txt):
     txt = re.sub("[^a-zA-Z ]", "", str(txt))
     txt = txt.lower()
@@ -34,17 +37,26 @@ def clean(txt):
 token_data = data['abstract'].apply(clean)
 
 #Using gensim to generate model below
-#I'm unsure of whether using the token_data as the corpus
-#is the most efficient approach without any windowing or size
-model = gensim.models.Word2Vec(token_data, size = 100)
-
+model = gensim.models.Word2Vec(token_data, size = 100, window= 20, min_count= 15000, workers= 4)
 #Save for future use
 model.save("w2v.model")
 
-#Below section is just to test that we've read in correctly
-five_word = [x for x in model.wv.vocab][0:5]
-print(five_word)
+#Trying to get the word label
+vocab = list(model.wv.vocab)
+X = model[vocab]
 
+tsne = TSNE(n_components= 2)
+X_tsne = tsne.fit_transform(X)
 
+#Loading into pandas Dataframe
+df = pd.DataFrame(X_tsne, index=vocab, columns=['x', 'y'])
 
+#plotting 
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+ax.scatter(df['x'], df['y'])
+
+for word, pos in df.iterrows():
+    ax.annotate(word, pos)
+plt.show()
 
