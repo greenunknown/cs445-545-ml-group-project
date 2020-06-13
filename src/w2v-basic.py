bbb62ac7353aa851e75a86cd6ae3cf7b9047761f
numberTuu <65688015+numberTuu@users.noreply.github.com>
2020-06-12 20:04:39 -0700

    Update and rename gensim-test.py to w2v-basic.py

--- src/w2v-basic.py
+++ src/w2v-basic.py
@@ -7,13 +7,16 @@ import nltk
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 
+#Uncomment this on first run to make sure you have the 
+#necessary files for nltk
 #nltk.download('punkt') 
 
 #Using the same method as Angie's LDA to filter and preprocess 
 #the csv into corpus.  Credit to Angie McGraw
 
-#NOTICE: UNCOMMENT THE SECTION BELOW ON FIRST RUN
-
+##########################################################################
+###########NOTICE: UNCOMMENT THE SECTION BELOW ON FIRST RUN###############
+##########################################################################
 """
 metadata = pd.read_csv('metadata.csv', low_memory= False)
 keep_columns = ['abstract', 'publish_time']
@@ -27,36 +30,34 @@ data.to_csv("newmeta.csv", index = False)
 data = pd.read_csv('newmeta.csv', low_memory= False)
 
 #Remove punctuation and lower case all characters
-#No removal of numbers
+#No removal of numbers or stop words.
 def clean(txt):
-    txt = re.sub("[^a-zA-Z ]", "", str(txt))
+    txt = re.sub("[^a-zA-Z0-9 ]", "", str(txt))
     txt = txt.lower()
     txt = nltk.word_tokenize(txt)
     return txt
 
+#Apply minor cleaing to the data
 token_data = data['abstract'].apply(clean)
 
 #Using gensim to generate model below
 model = gensim.models.Word2Vec(token_data, size = 100, window= 20, min_count= 15000, workers= 4)
-#Save for future use
-model.save("w2v.model")
+
+#print(np.array(model.wv.most_similar('patient')))
 
 #Trying to get the word label
 vocab = list(model.wv.vocab)
-X = model[vocab]
-
-tsne = TSNE(n_components= 2)
+X = model.wv.__getitem__(vocab)
+tsne = TSNE(perplexity= 40, n_components= 2, init= 'pca', n_iter= 2500, random_state= 23)
 X_tsne = tsne.fit_transform(X)
 
 #Loading into pandas Dataframe
 df = pd.DataFrame(X_tsne, index=vocab, columns=['x', 'y'])
 
-#plotting 
+#plotting from the pandas Dataframe
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
 ax.scatter(df['x'], df['y'])
-
 for word, pos in df.iterrows():
     ax.annotate(word, pos)
 plt.show()
-
